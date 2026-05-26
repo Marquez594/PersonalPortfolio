b35c76ba2fb4e97cdc158e86e165bbc817bd6978
Marquez594 <bmarquez1594@gmail.com>
2026-05-25 20:38:57 -0400

    Added bulletin Board project

--- src/data/dataProject.ts
+++ src/data/dataProject.ts
@@ -28,6 +28,13 @@ export const projectList: projectType[] = [
     name: "Weather App",
     description: "Weather App",
     github: "https://github.com/Marquez594/WeatherApp",
-    page: "https://marquez594.github.io/WeatherApp/"
-  }
+    page: "https://marquez594.github.io/WeatherApp/",
+  },
+  {
+    name: "Bulletin-Board",
+    description:
+      "Site where users can send meessage to everyone using a database. Full stack application with node.js in the backend",
+    github: "https://github.com/Marquez594/Bulletin-Board",
+    page: "https://bulletin-delta-seven.vercel.app/",
+  },
 ];

2597f321031415874d29d83e6346f7e5e6fe55c8
Marquez594 <bmarquez1594@gmail.com>
2026-06-18 14:16:28 -0400

    Fixed desc

--- src/data/dataProject.ts
+++ src/data/dataProject.ts
@@ -8,25 +8,29 @@ export type projectType = {
 export const projectList: projectType[] = [
   {
     name: "Pokemon Search",
-    description: "Website",
+    description:
+      "A site where the user can look up their favorite Pokémon and grab valuable information about them. This includes making API calls to get information about the Pokémon, the ability of the Pokémon, and the evolution chain.",
     github: "https://github.com/Marquez594/PokemonAPI",
     page: "https://marquez594.github.io/PokemonAPI/",
   },
   {
     name: "Quiz App",
-    description: "Quiz App",
+    description:
+      "A simple quiz site that lets the user answer and see whether or not their answer was correct",
     github: "https://github.com/Marquez594/QuizApp",
     page: "https://marquez594.github.io/QuizApp/",
   },
   {
     name: "E-com Website",
-    description: "Buy all your fav stuff",
+    description:
+      "Made a website that users could browse and “shop” using an API to get the information. It had simple filters and different ways to sort the items.",
     github: "https://github.com/Marquez594/E-Com",
     page: "https://marquez594.github.io/E-Com/",
   },
   {
     name: "Weather App",
-    description: "Weather App",
+    description:
+      "Using API's, I would have a weather show for a location. The browser asks for permission for the user's location to check the weather. Users could also check other locations if they wished ",
     github: "https://github.com/Marquez594/WeatherApp",
     page: "https://marquez594.github.io/WeatherApp/",
   },
